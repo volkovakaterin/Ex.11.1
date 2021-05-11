5ea217810d472be14ebf3affbca1a845d8afbe7f
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-05-11 23:57:09 +0400

    files changed

--- src/js/team.js
+++ src/js/team.js
@@ -4,54 +4,56 @@
 /* eslint-disable no-sequences */
 export default class Team {
   constructor() {
-    this.magician = {
-      name: 'Magician',
-      type: 'Magician',
-      health: 100,
-      level: 1,
-      attack: 100,
-      defence: 40,
-    },
-    this.daemon = {
-      name: 'Daemon',
-      type: 'Daemon',
-      health: 100,
-      level: 1,
-      attack: 100,
-      defence: 40,
-    },
-    this.bowman = {
-      name: 'Bowman',
-      type: 'Bowman',
-      health: 0,
-      level: 1,
-      attack: 25,
-      defence: 25,
-    },
-    this.swordsman = {
-      name: 'Swordsman',
-      type: 'Swordsman',
-      health: -5,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    },
-    this.undead = {
-      name: 'Undead',
-      type: 'Undead',
-      health: 100,
-      level: 1,
-      attack: 25,
-      defence: 25,
-    };
+    this.members = [
+      {
+        name: 'Magician',
+        type: 'Magician',
+        health: 100,
+        level: 1,
+        attack: 100,
+        defence: 40,
+      },
+      {
+        name: 'Daemon',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        attack: 100,
+        defence: 40,
+      },
+      {
+        name: 'Bowman',
+        type: 'Bowman',
+        health: 0,
+        level: 1,
+        attack: 25,
+        defence: 25,
+      },
+      {
+        name: 'Swordsman',
+        type: 'Swordsman',
+        health: -5,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Undead',
+        type: 'Undead',
+        health: 100,
+        level: 1,
+        attack: 25,
+        defence: 25,
+      },
+    ];
   }
 
   [Symbol.iterator]() {
-    const persons = Object.values(this);
+    const { members } = this;
     let currentPersonIndex = 0;
     return {
       next() {
-        const doNotHaveMorePerson = !(currentPersonIndex < persons.length);
+        const doNotHaveMorePerson = !(currentPersonIndex < members.length);
         if (doNotHaveMorePerson) {
           return {
             value: undefined,
@@ -59,7 +61,7 @@ export default class Team {
           };
         }
         return {
-          value: persons[currentPersonIndex++],
+          value: members[currentPersonIndex++],
           done: false,
         };
       },
